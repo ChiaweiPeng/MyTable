e22c3181e83b643850450df39e9958823c8a5229
Pengchiawei <chiawei429789881@163.com>
2021-06-28 19:26:42 +0800

    改用array.includes/filter方法

--- src/utils/getData.js
+++ src/utils/getData.js
@@ -7,13 +7,12 @@ function getData(searchData) {
         if (searchData === 1) {
             arr = productData
         } else {
-            productData.forEach(item => {
-                // TODO1: 查阅 MDN 文档: Array.includes / String.includes语法，在此场景下代替indexOf方法
-                // TODO2: 查阅 MDN 文档: Array.filter语法，代替let arr = [] + arr.forEach + arr.push的写法
-                if (item.product.indexOf(searchData.product) >= 0 || item.type.indexOf(searchData.type) >= 0 || item.name.indexOf(searchData.prodName) >= 0) {
-                    arr.push(item)
-                }
+            // TODO1: 查阅 MDN 文档: Array.includes / String.includes语法，在此场景下代替indexOf方法
+            // TODO2: 查阅 MDN 文档: Array.filter语法，代替let arr = [] + arr.forEach + arr.push的写法
+            arr = productData.filter(item => {
+                return item.product.includes(searchData.product) || item.type.includes(searchData.type) || item.name.includes(searchData.prodName)
             })
+
             if (searchData.prodName !== " " && arr.length === 0) {
                 arr = productData
                 message.info('您搜索的内容不存在,为您展示当前数据')
